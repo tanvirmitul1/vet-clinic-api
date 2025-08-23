5b7afcbc145da987663c24b4422d0be0858a5536
tanvirmitul1 <mitulseopage1@gmail.com>
2025-08-23 18:07:36 +0600

    config added

---
+++ config/cors.php
@@ -0,0 +1,17 @@
+<?php
+
+return [
+    'paths' => ['api/*'],
+    'allowed_methods' => ['*'],
+    'allowed_origins' => [
+        'http://localhost:3000',
+        'http://127.0.0.1:8000',
+        'http://127.0.0.1:8001',
+        'https://www.tanvirmitul.com',
+    ],
+    'allowed_origins_patterns' => [],
+    'allowed_headers' => ['*'],
+    'exposed_headers' => [],
+    'max_age' => 0,
+    'supports_credentials' => true, // Important for sending cookies
+];
