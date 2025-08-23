cf4c9f81da8bb3bfb19fc8fefc6e7af4c565f21b
tanvirmitul1 <mitulseopage1@gmail.com>
2025-08-23 23:57:26 +0600

    middlewares added in bootstrap app file

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -6,13 +6,25 @@ use Illuminate\Foundation\Configuration\Middleware;
 
 return Application::configure(basePath: dirname(__DIR__))
     ->withRouting(
-        web: __DIR__.'/../routes/web.php',
-        commands: __DIR__.'/../routes/console.php',
+        web: __DIR__ . '/../routes/web.php',
+        api: __DIR__ . '/../routes/api.php',
+        commands: __DIR__ . '/../routes/console.php',
         health: '/up',
     )
-    ->withMiddleware(function (Middleware $middleware): void {
-        //
+    ->withMiddleware(function (Middleware $middleware) {
+        // 🔹 Add your middleware alias here
+        $middleware->alias([
+            'jwt.cookie' => \App\Http\Middleware\JwtFromCookie::class,
+            'verified' => \Illuminate\Auth\Middleware\EnsureEmailIsVerified::class,
+        ]);
+
+        // If you want to add it to a middleware group
+        $middleware->appendToGroup('api', [
+            // \Laravel\Sanctum\Http\Middleware\EnsureFrontendRequestsAreStateful::class,
+            // 'throttle:api',
+            // \Illuminate\Routing\Middleware\SubstituteBindings::class,
+        ]);
     })
-    ->withExceptions(function (Exceptions $exceptions): void {
+    ->withExceptions(function (Exceptions $exceptions) {
         //
     })->create();
